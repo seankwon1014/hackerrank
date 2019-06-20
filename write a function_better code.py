b3a512ecd5f7830f22ca2bfa4ae6362583a187fa
Sihyun Kwon <46009326+seankwon1014@users.noreply.github.com>
2019-06-20 02:05:18 -0400

    Delete write a function_better code.py

--- write a function_better code.py
+++
@@ -1,5 +0,0 @@
-from itertools import groupby
-print(*[(len(list(c)), int(x)) for x, c in groupby(input())])
-# x brings each value based on group by
-# c brings the location of x: list(c) brings the bunch of x
-# if don't use **int** in int(x), it brings string like '1'
